b2cd65415458f7f79d01136e2d2b43106d9bc004
Subha Sundar Chakraborty <72214531+csubhasundar@users.noreply.github.com>
2022-04-23 20:23:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter Name");
+ var b=prompt("Enter Date of Birth");
+  	if(a== "Subha " && b=="27-12-2000")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("Well Come");
+  		window.location.href="start.html";
+  	}
+  	
